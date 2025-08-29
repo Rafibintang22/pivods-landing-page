f98579af1b7408f81f2dfa958276b7e4fb74c948
Rafibintang22 <rafibintang26.rb@gmail.com>
2025-08-29 20:57:56 +0700

    update :

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { cn } from "@/lib/utils";
 import Nav from "@/components/Nav";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import Image from "next/image";
@@ -8,17 +9,35 @@ export default function About() {
         <>
             <Nav />
             <main>
-                <section className="min-h-[30vh]"></section>
-                <section className="relative w-full h-[150px] bg-white">
+                <section className="min-h-[35vh] lg:min-h-[50vh] w-full flex justify-center items-center">
                     <Image
-                        src="/wave-bottom.png"
-                        alt="wave-img"
+                        src="/Wave-line.png"
+                        alt="background-wave"
                         fill
-                        className="object-cover"
+                        className="object-cover -z-10 opacity-20"
                         priority
                     />
+                    {/* <div
+                        className={cn(
+                            "absolute inset-0 -z-20",
+                            "[background-size:40px_40px] opacity-50"
+                        )}
+                        style={{
+                            backgroundImage: `linear-gradient(to right, color-mix(in srgb, var(--primary) 30%, transparent) 1px, transparent 1px),
+                          linear-gradient(to bottom, color-mix(in srgb, var(--primary) 30%, transparent) 1px, transparent 1px)`,
+                        }}
+                    /> */}
+
+                    <div className="flex flex-col items-center px-3">
+                        <h1 className="text-3xl md:text-5xl font-bold mb-3 text-gray-900 dark:text-gray-100">
+                            About
+                        </h1>
+                        <p className="font-semibold text-center">
+                            Let’s deepen our introduction — would you like to start?
+                        </p>
+                    </div>
                 </section>
-                <section className="bg-white h-screen"></section>
+                <section className="bg-zinc-900 h-screen"></section>
             </main>
             <WhatsAppButton />
         </>
--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -15,7 +15,7 @@ function Providers({ children }) {
     }
 
     return (
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             {children}
         </ThemeProvider>
     );
