7dfca2fd086645632bd1869a0b34acd97fe79eb6
Jasonnoi <103072996+Jasonnoi@users.noreply.github.com>
2025-08-31 20:32:03 +0700

    asdasd

--- src/components/ui/sticky-scroll-reveal.jsx
+++ src/components/ui/sticky-scroll-reveal.jsx
@@ -4,111 +4,119 @@ import { useMotionValueEvent, useScroll, motion } from "motion/react";
 import { GlowingEffect } from "./glowing-effect";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPeace } from "@fortawesome/free-solid-svg-icons";
-
+import { Button } from "../global";
+import { ArrowUpRight } from "lucide-react";
 export const StickyScroll = ({ content, contentClassName }) => {
-    const [activeCard, setActiveCard] = useState(0);
-    const containerRef = useRef(null);
+	const [activeCard, setActiveCard] = useState(0);
+	const containerRef = useRef(null);
 
-    // Scroll relatif ke container section
-    const { scrollYProgress } = useScroll({
-        target: containerRef,
-        offset: ["start start", "end end"], // scroll 0-1 relatif section
-    });
+	// Scroll relatif ke container section
+	const { scrollYProgress } = useScroll({
+		target: containerRef,
+		offset: ["start start", "end end"], // scroll 0-1 relatif section
+	});
 
-    const cardLength = content.length;
+	const cardLength = content.length;
 
-    useMotionValueEvent(scrollYProgress, "change", (latest) => {
-        const breakpoints = content.map((_, index) => index / cardLength);
+	useMotionValueEvent(scrollYProgress, "change", (latest) => {
+		const step = 1 / (cardLength - 1); // jarak antar card
+		const breakpoints = content.map((_, index) => index * step);
 
-        const closestIndex = breakpoints.reduce((acc, bp, idx) => {
-            return Math.abs(latest - bp) < Math.abs(latest - breakpoints[acc]) ? idx : acc;
-        }, 0);
+		const closestIndex = breakpoints.reduce((acc, bp, idx) => {
+			return Math.abs(latest - bp) < Math.abs(latest - breakpoints[acc])
+				? idx
+				: acc;
+		}, 0);
 
-        setActiveCard(closestIndex);
-    });
+		setActiveCard(closestIndex);
+	});
 
-    return (
-        <motion.div
-            ref={containerRef}
-            className="relative flex flex-col lg:flex-row justify-between gap-5 mt-12"
-        >
-            <div className="w-full lg:max-w-2xl flex-1">
-                {content.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`${index !== content.length - 1 ? "mb-20 lg:mb-52" : ""}`}
-                    >
-                        <motion.h2
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: activeCard === index ? 1 : 0.3 }}
-                            className="text-md md:text-xl font-bold text-slate-100"
-                        >
-                            {item.title}
-                        </motion.h2>
-                        <motion.p
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: activeCard === index ? 1 : 0.3 }}
-                            className="text-xs mt-3 max-w-full md:max-w-sm text-slate-300 leading-relaxed"
-                        >
-                            {item.description}
-                        </motion.p>
+	return (
+		<motion.div
+			ref={containerRef}
+			className="relative flex justify-between gap-5 mt-12"
+		>
+			<div className="w-4/6 flex-1">
+				{content.map((item, index) => (
+					<div
+						key={index}
+						className={`${
+							index !== content.length - 1 ? "mb-52" : ""
+						} flex flex-col justify-center h-[30rem] `}
+					>
+						<span className="bg-primary/20 text-primary h-fit w-fit px-4 py-2 rounded-full text-[15px] backdrop-blur-md">
+							{item.title}
+						</span>
+						<motion.h2
+							initial={{ opacity: 0 }}
+							animate={{
+								opacity: activeCard === index ? 1 : 0.3,
+							}}
+							className="text-4xl max-w-4xl  font-bold my-6 text-left text-white dark:text-gray-100"
+						>
+							{item?.hook || "Not Yet Available"}
+						</motion.h2>
+						<motion.p
+							initial={{ opacity: 0 }}
+							animate={{
+								opacity: activeCard === index ? 1 : 0.3,
+							}}
+							className="text-kg max-w-3xl text-slate-300"
+						>
+							{item.description}
+						</motion.p>
+						<motion.div
+							initial={{ opacity: 0 }}
+							animate={{
+								opacity: activeCard === index ? 1 : 0.3,
+							}}
+							className=" text-slate-300"
+						>
+							<div className="max-w-3xl mt-5 grid grid-cols-2 gap-y-2 gap-x-5">
+								{item?.usage.map((usg, index) => (
+									<div
+										key={index}
+										className="flex gap-2 items-center"
+									>
+										{usg.icon}
+										{usg.name}
+									</div>
+								))}
+							</div>
+							<div className="flex items-center gap-2 mt-8">
+								<Button className="px-6 py-3 w-fit  rounded-xl bg-primary hover:bg-primary/80 text-white shadow-lg transition">
+									Explore More
+								</Button>
+								<div className="bg-primary-3 text-white p-3 rounded-full shadow-md">
+									<ArrowUpRight size={20} />
+								</div>
+							</div>
+						</motion.div>
+					</div>
+				))}
+			</div>
 
-                        {/* Sticky Card for mobile/tablet */}
-                        <div className="mt-6 block lg:hidden">
-                            <div className="relative rounded-xl border p-2 md:rounded-3xl md:p-4">
-                                <GlowingEffect
-                                    blur={0}
-                                    borderWidth={3}
-                                    spread={80}
-                                    glow={true}
-                                    disabled={false}
-                                    proximity={64}
-                                    inactiveZone={0.01}
-                                />
-                                <div className="bg-white/10 backdrop-blur-[2px] relative flex flex-col gap-6 overflow-hidden rounded-3xl p-4 md:p-6 dark:shadow-[0px_0px_27px_0px_#2D2D2D]">
-                                    <div className="w-fit rounded-lg border border-gray-600 p-2">
-                                        <FontAwesomeIcon icon={faPeace} />
-                                    </div>
-                                    <h2 className="text-sm md:text-base dark:text-neutral-400 [&_b]:font-semibold [&_strong]:font-semibold">
-                                        {item.content ?? null}
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            <div className="sticky top-0 left-0">
-                <div className="absolute inset-0 bg-primary/50 blur-[200px] -z-10"></div>
-                <div
-                    className={`mt-[48px] sticky top-52 hidden h-100 w-80 rounded-xl lg:block overflow-hidden list-none ${contentClassName}`}
-                >
-                    <div className="relative h-full rounded-xl border p-1 md:rounded-3xl md:p-2">
-                        <GlowingEffect
-                            blur={0}
-                            borderWidth={3}
-                            spread={80}
-                            glow={true}
-                            disabled={false}
-                            proximity={64}
-                            inactiveZone={0.01}
-                        />
-                        <div className="bg-white/10 backdrop-blur-[2px] border-0.75 relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-3xl p-6 md:p-6 dark:shadow-[0px_0px_27px_0px_#2D2D2D]">
-                            <div className="relative flex flex-1 flex-col justify-between gap-3">
-                                <div className="w-fit rounded-lg border border-gray-600 p-2">
-                                    <FontAwesomeIcon icon={faPeace} />
-                                </div>
-                                <div className="space-y-3">
-                                    <h2 className="text-sm/[1.125rem] text-black md:text-base/[1.375rem] dark:text-neutral-400 [&_b]:md:font-semibold [&_strong]:md:font-semibold">
-                                        {content[activeCard].content ?? null}
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </motion.div>
-    );
+			<div className="sticky top-0 w-2/6 left-0">
+				<div className="absolute inset-0 bg-primary/50 blur-[200px] -z-10"></div>
+				<div
+					className={`mt-[48px] sticky top-52 hidden h-100 w-full lg:block overflow-hidden list-none ${contentClassName}`}
+				>
+					<div className="relative h-full  p-1 md:p-2">
+						<GlowingEffect
+							blur={0}
+							borderWidth={3}
+							spread={80}
+							glow={true}
+							disabled={false}
+							proximity={64}
+							inactiveZone={0.01}
+						/>
+						<div className="w-[90%] h-full relative">
+							{content[activeCard].content ?? null}
+						</div>
+					</div>
+				</div>
+			</div>
+		</motion.div>
+	);
 };
